15a2cd9e62857b76e0494deec2de9a33e08aedd3
Doozer <D0023R@users.noreply.github.com>
2023-04-27 18:56:52 +0200

    Add Pico Black clone board

--- Firmware/LogicAnalyzer/LogicAnalyzer_Board_Settings.h
+++ Firmware/LogicAnalyzer/LogicAnalyzer_Board_Settings.h
@@ -80,6 +80,19 @@
         #define LED_IO 16
         //#define USE_CYGW_WIFI
 
+    #elif defined (BUILD_PICO_BLACK)
+
+        #define BOARD_NAME "PICO_BLACK"
+        #define SUPPORTS_COMPLEX_TRIGGER
+        #define INPUT_PIN_BASE 2
+        #define COMPLEX_TRIGGER_OUT_PIN 0
+        #define COMPLEX_TRIGGER_IN_PIN 1
+        #define GPIO_LED
+        //#define CYGW_LED
+        //#define WS2812_LED
+        #define LED_IO 25
+        //#define USE_CYGW_WIFI
+
     #endif
 
 #endif
